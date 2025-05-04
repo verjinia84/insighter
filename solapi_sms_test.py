be82ffd282c88521f972042ec892a851dbd23805
verjinia84 <78085704+verjinia84@users.noreply.github.com>
2025-05-05 03:17:22 +0900

    Update solapi_sms_test.py

--- solapi_sms_test.py
+++ solapi_sms_test.py
@@ -1,12 +1,13 @@
 import json
 import logging
 import sys
+import os
 from solapi import SolapiMessageService
 from solapi.model.message import Message
 
 # 환경 변수 또는 secrets로 관리하는 것이 안전합니다.
-API_KEY = 'NCSKTQHYNGTGMQNW'
-API_SECRET = 'NZDS6GWKHV7L6962MDRFTSAHVD4PHLEZ'
+API_KEY = os.environ.get('SOLAPI_API_KEY')
+API_SECRET = os.environ.get('SOLAPI_API_SECRET')
 
 def send_bulk_sms(json_path):
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
